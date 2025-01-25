581668585a9e33d714bd536f7f3501ede8e65cfb
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-01-25 18:01:13 -0500

    Rename makeStorageMiddleware to makeFikaMiddleware

--- apps/fika-demo-001/src/server.ts
+++ apps/fika-demo-001/src/server.ts
@@ -1,8 +1,8 @@
 import { handler } from "@fika-ts/framework/handler.server";
+import { importMapDefaults } from "@fika-ts/framework/import-map-defaults";
 import { makeFikaMiddleware } from "@fika-ts/framework/storage.server";
 import { Hono } from "hono";
 import { routes } from "./routes.gen";
-
 let app = new Hono();
 
 app.use(
@@ -11,11 +11,7 @@ app.use(
     importMap: {
       imports: {
         // add `?dev` to the esm urls for dev assets
-        react: "https://esm.sh/react@experimental",
-        "react/jsx-runtime": "https://esm.sh/react@experimental/jsx-runtime",
-        "react-dom": "https://esm.sh/react-dom@experimental",
-        "react-dom/client": "https://esm.sh/react-dom@experimental/client",
-        "#framework/storage": "/storage.client.js",
+        ...importMapDefaults,
       },
     },
     bootstrapModules: ["/routes.gen.js", "/entry.client.js"],
---
+++ packages/fika-framework/src/import-map-defaults.ts
@@ -0,0 +1,8 @@
+export let importMapDefaults: Record<string, string> = {
+  react: "https://esm.sh/react@experimental",
+  "react/jsx-runtime": "https://esm.sh/react@experimental/jsx-runtime",
+  "react-dom": "https://esm.sh/react-dom@experimental",
+  "react-dom/client": "https://esm.sh/react-dom@experimental/client",
+  // Special generated file during builds
+  "#framework/storage": "/storage.client.js",
+};
